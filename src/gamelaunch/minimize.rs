da7ddef62a520e2dd65ac01d6752d7ad8bcb3eb0
DI3D <DI3D@users.noreply.github.com>
2022-09-26 19:35:20 -0400

    update to minimize but from_utf8 errors

--- src/gamelaunch/minimize.rs
+++ src/gamelaunch/minimize.rs
@@ -4,17 +4,23 @@ use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextA, EnumWindows, WNDEN
 
 pub fn toggle_rl_windows(minimize: bool) {
     if minimize {
-        // let window_ledger: HashMap<isize, String> = HashMap::new();
-        // String::from_utf8(vec)
 
         unsafe extern "system" fn win_enum_handler(hwnd: HWND, param1: LPARAM)  -> BOOL {
             let window_visible: bool;
             window_visible = IsWindowVisible(hwnd).as_bool();
 
             if window_visible {
-                let mut window_u8: Vec<u8> = Vec::new();
-                GetWindowTextA(hwnd, window_u8.as_mut_slice());
-                let window_text = String::from_utf8(window_u8).unwrap();
+                let mut window_u8: Vec<u8> = vec![0; 500];
+                let out = GetWindowTextA(hwnd, window_u8.as_mut_slice());
+                window_u8.truncate(out as usize);
+                let window_text = String::from_utf8(window_u8.clone());
+                let window_text = match window_text {
+                    Ok(window_text) => window_text,
+                    Err(err) => {println!("FromUtf8Error: {err}");
+                    let window_u8_str = window_u8.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" "); 
+                    println!("u8 line: {window_u8_str}");
+                    String::new()}
+                };
                 let out;
                 if &window_text == "Rocket League" {
                     out = ShowWindow(hwnd, SW_FORCEMINIMIZE);
