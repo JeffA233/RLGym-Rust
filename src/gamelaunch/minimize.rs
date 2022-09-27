23dd4d684e218f853b2843435cf7f240c0d7a501
DI3D <DI3D@users.noreply.github.com>
2022-09-26 21:28:19 -0400

    fixed minimize

--- src/gamelaunch/minimize.rs
+++ src/gamelaunch/minimize.rs
@@ -1,5 +1,5 @@
 use windows::Win32::Foundation::{HWND, LPARAM, BOOL};
-use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextA, EnumWindows, WNDENUMPROC, IsWindowVisible, ShowWindow, SW_FORCEMINIMIZE};
+use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextW, EnumWindows, WNDENUMPROC, IsWindowVisible, ShowWindow, SW_FORCEMINIMIZE};
 
 
 pub fn toggle_rl_windows(minimize: bool) {
@@ -10,19 +10,20 @@ pub fn toggle_rl_windows(minimize: bool) {
             window_visible = IsWindowVisible(hwnd).as_bool();
 
             if window_visible {
-                let mut window_u8: Vec<u8> = vec![0; 500];
-                let out = GetWindowTextA(hwnd, window_u8.as_mut_slice());
-                window_u8.truncate(out as usize);
-                let window_text = String::from_utf8(window_u8.clone());
+                let mut window_u16: Vec<u16> = vec![0; 500];
+                let out = GetWindowTextW(hwnd, window_u16.as_mut_slice());
+                window_u16.truncate(out as usize);
+                let window_text = String::from_utf16(window_u16.clone().as_slice());
                 let window_text = match window_text {
                     Ok(window_text) => window_text,
                     Err(err) => {println!("FromUtf8Error: {err}");
-                    let window_u8_str = window_u8.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" "); 
-                    println!("u8 line: {window_u8_str}");
+                    let window_u8_str = window_u16.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" "); 
+                    // println!("u8 line: {window_u8_str}");
                     String::new()}
                 };
+                println!("window text: {window_text}");
                 let out;
-                if &window_text == "Rocket League" {
+                if window_text.contains("Rocket League") {
                     out = ShowWindow(hwnd, SW_FORCEMINIMIZE);
                     println!("Rocket League is now minimized");
                     return BOOL::from(true)
